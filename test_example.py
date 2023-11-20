5257b6098867859ace26aa1326a7868309f288f2
yx135 <120313253+yx135@users.noreply.github.com>
2023-11-20 20:06:05 +0800

    Create test_example.py

---
+++ test_example.py
@@ -0,0 +1,9 @@
+import pytest
+def test_addition1():
+    assert 1 + 1 == 2
+def test_addition2():
+    assert 1 + 1 == 2
+def test_addition3():
+    assert 1 + 1 == 2
+def test_addition4():
+    assert 1 + 1 == 2
